25072c5661dde61531907d9591f103e2eda22c95
Vijayram Gopu <vgopu@vmware.com>
2014-10-03 11:37:16 -0700

    Changed Employee added social security

--- src/test/java/com/vmware/EmployeeTest.java
+++ src/test/java/com/vmware/EmployeeTest.java
@@ -1,13 +1,22 @@
 package com.vmware;
 
 import static org.testng.Assert.*;
+
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class EmployeeTest {
-
+		
+		private Employee employee;
+		
+		@BeforeMethod
+		public void setUp(){
+			System.out.println("Construct a new Emplyee per test");
+			employee = new Employee();
+		}
+		
 		@Test
 		public void createEmployeeAndGetName() {
-			Employee employee = new Employee();
 			String name = "Ricardo Montalban";
 			employee.setName(name);
 			assertEquals(employee.getName(), name);
@@ -19,14 +28,12 @@ public class EmployeeTest {
 		// F3 Jump to Source
 		@Test
 		public void addSocialSecurityNumber(){
-			Employee employee = new Employee();
 			employee.setSocialSecurityNumber("123-45-6789");
 			assertEquals(employee.getSocialSecurityNumber(), "123-45-6789");
 		}
 		
 		@Test
 		public void addAnotherSocialSecurityNumber(){
-			Employee employee = new Employee();
 			employee.setSocialSecurityNumber("123-45-6710");
 			assertEquals(employee.getSocialSecurityNumber(), "123-45-6710");
 		}
@@ -34,7 +41,6 @@ public class EmployeeTest {
 		@Test
 		public void fixDE30201_SocialSecurityMustBeACertainFormat(){
 			String badSSN = "RamLikesThe49ers";
-			Employee employee = new Employee();
 			try{
 				employee.setSocialSecurityNumber(badSSN);
 				fail("This linke should never have been reached...");
